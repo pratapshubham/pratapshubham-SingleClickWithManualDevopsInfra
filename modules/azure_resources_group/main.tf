fc4e1d8abd7a6de6227edc6b08fdf07a86f71153
theShubhamOnly <shubham@queuebuster.co>
2026-07-29 14:21:18 +0530

    Minor changes

--- modules/azure_resources_group/main.tf
+++ modules/azure_resources_group/main.tf
@@ -2,8 +2,4 @@ resource "azurerm_resource_group" "rg1" {
     for_each = var.rgvalue
     name = each.value.name
     location = each.value.location
-
-    tags = {
-        environment = each.value.environment
-    }
 }
